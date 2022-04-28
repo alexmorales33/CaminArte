6db5f1f087e4b059ad52f23e4b954a5885296e15
alexmorales33 <alexmoraleess4@gmail.com>
2022-04-28 13:57:46 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+const search = () => {
+  const searchbox = document.getElementById("searchbox").value.toUpperCase();
+  const storeitem = document.getElementById("product-list");
+  const product = document.querySelectorAll(".edificio-arquitecto")
+  const pname = storeitem.getElementsByTagName("h2")
+  
+
+  for(var i=0; i < pname.length; i++) {
+    let match = product[i].getElementsByTagName('h2')[0];
+
+    if(match){
+      let textvalue = match.textContent || match.innerHTML
+
+      if(textvalue.toUpperCase().indexOf(searchbox) > -1){
+        product[i].style.display = "";
+        
+      } else {
+        product[i].style.display = "none";
+      }
+    }
+  }
+}
